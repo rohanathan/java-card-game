0caed3c5917f13e39888824c9c6fda3d6c709fcc
Xuechun Ji <luandun1018@gmail.com>
2025-03-12 18:44:27 +0000

    Refactored and Commented Unit

--- app/structures/basic/Unit.java
+++ app/structures/basic/Unit.java
@@ -29,43 +29,59 @@ public class Unit implements GameAction {
 	Position position;
 	UnitAnimationSet animations;
 	ImageCorrection correction;
-	// Keep track of the unit's current health
-	int health;
-	// Keep track of the unit's current attack
-	int attack;
-	// Keep track of the owner of the unit
+	int health;		// Keep track of the unit's current health
+	int attack;		// Keep track of the unit's current attack
 	@JsonBackReference
-	Player owner;
-	// Keep track of whether the unit has moved in the current turn
-	boolean hasMoved = false;
-	// Keep track of whether the unit has attacked in the current turn
-	boolean hasAttacked = false;
-
-	boolean hasProvoke = false;
+	Player owner;	// Keep track of the owner of the unit
+	boolean hasMoved = false;	// Keep track of whether the unit has moved in the current turn
+	boolean hasAttacked = false; 	// Keep track of whether the unit has attacked in the current turn
+	boolean hasProvoke = false; // Whether the unit has the Provoke ability
 	private int maxHealth;
 	public Unit() {}
 
+	/**
+	 * Constructor for initializing a unit with basic properties.
+	 *
+	 * @param id         The unique ID of the unit.
+	 * @param animations The animation set for the unit.
+	 * @param correction The image correction data for rendering.
+	 */
 	public Unit(int id, UnitAnimationSet animations, ImageCorrection correction) {
 		super();
 		this.id = id;
 		this.animation = UnitAnimationType.idle;
-
-		position = new Position(0, 0, 0, 0);
+		this.position = new Position(0, 0, 0, 0);
 		this.correction = correction;
 		this.animations = animations;
 	}
 
+	/**
+	 * Constructor for initializing a unit with a specific tile position.
+	 *
+	 * @param id          The unique ID of the unit.
+	 * @param animations  The animation set for the unit.
+	 * @param correction  The image correction data for rendering.
+	 * @param currentTile The tile where the unit is initially placed.
+	 */
 	public Unit(int id, UnitAnimationSet animations, ImageCorrection correction, Tile currentTile) {
 		super();
 		this.id = id;
 		this.animation = UnitAnimationType.idle;
-
-		position = new Position(currentTile.getXpos(), currentTile.getYpos(), currentTile.getTilex(),
+		this.position = new Position(currentTile.getXpos(), currentTile.getYpos(), currentTile.getTilex(),
 				currentTile.getTiley());
 		this.correction = correction;
 		this.animations = animations;
 	}
 
+	/**
+	 * Constructor for initializing a unit with all properties.
+	 *
+	 * @param id         The unique ID of the unit.
+	 * @param animation  The current animation type.
+	 * @param position   The position of the unit on the board.
+	 * @param animations The animation set for the unit.
+	 * @param correction The image correction data for rendering.
+	 */
 	public Unit(int id, UnitAnimationType animation, Position position, UnitAnimationSet animations,
 			ImageCorrection correction) {
 		super();
@@ -79,7 +95,6 @@ public class Unit implements GameAction {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -128,35 +143,74 @@ public class Unit implements GameAction {
 		this.health = health;
 	}
 
+	/**
+	 * Gets the attack value of the unit.
+	 *
+	 * @return The attack value of the unit.
+	 */
 	public int getAttack() {
 		return attack;
 	}
 
+	/**
+	 * Sets the attack value of the unit.
+	 *
+	 * @param attack The new attack value to set.
+	 */
 	public void setAttack(int attack) {
 		this.attack = attack;
 	}
 
-
+	/**
+	 * Gets the owner of the unit.
+	 *
+	 * @return The player who owns the unit.
+	 */
 	public Player getOwner() {
 		return owner;
 	}
 
+	/**
+	 * Sets the owner of the unit.
+	 *
+	 * @param owner The player to set as the owner of the unit.
+	 */
 	public void setOwner(Player owner) {
 		this.owner = owner;
 	}
 
+	/**
+	 * Checks if the unit has moved in the current turn.
+	 *
+	 * @return True if the unit has moved, false otherwise.
+	 */
 	public boolean hasMoved() {
 		return hasMoved;
 	}
 
+	/**
+	 * Sets whether the unit has moved in the current turn.
+	 *
+	 * @param hasMoved True if the unit has moved, false otherwise.
+	 */
 	public void setHasMoved(boolean hasMoved) {
 		this.hasMoved = hasMoved;
 	}
 
+	/**
+	 * Checks if the unit has attacked in the current turn.
+	 *
+	 * @return True if the unit has attacked, false otherwise.
+	 */
 	public boolean hasAttacked() {
 		return hasAttacked;
 	}
 
+	/**
+	 * Sets whether the unit has attacked in the current turn.
+	 *
+	 * @param hasAttacked True if the unit has attacked, false otherwise.
+	 */
 	public void setHasAttacked(boolean hasAttacked) {
 		this.hasAttacked = hasAttacked;
 	}
@@ -171,6 +225,12 @@ public class Unit implements GameAction {
 		position = new Position(tile.getXpos(), tile.getYpos(), tile.getTilex(), tile.getTiley());
 	}
 
+	/**
+	 * Returns a string representation of the Unit object.
+	 * The string includes the unit's ID, name, position, owner, and whether it has moved.
+	 *
+	 * @return A string representation of the Unit.
+	 */
 	@Override
 	public String toString() {
 		return "Unit [id=" + id + ", name=" + name + ", position=" + position + ", owner=" + owner + ", hasMoved=" + hasMoved + "]";
@@ -183,11 +243,11 @@ public class Unit implements GameAction {
 
 	public int getMaxHealth() {
 		// TODO Auto-generated method stub
-		return this.maxHealth;
+		return this.maxHealth; // Return the maximum health of the unit
 	}
 	
-	public int setMaxHealth(int maxHealth) {
-		return this.maxHealth = maxHealth;
-	}
+	public int updateMaxHealth(int newMaxHealth) {
+		return this.maxHealth = newMaxHealth;
+	} // Set the maximum health of the unit
 
 }
