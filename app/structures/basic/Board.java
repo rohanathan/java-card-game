eccc0fb77da71e443091ee44e6e4209f64c38bbc
Xuechun Ji <luandun1018@gmail.com>
2025-03-12 02:29:25 +0000

    Refactored Board

--- app/structures/basic/Board.java
+++ app/structures/basic/Board.java
@@ -8,23 +8,27 @@ import java.util.Set;
 public class Board {
     private Tile[][] tiles = new Tile[9][5];;
 
-
     public Tile getTile(int x, int y) {
         return tiles[x][y];
     }
 
-    public void setTile(Tile tile, int x, int y) {
-        tiles[x][y] = tile;
-    }
+	public Tile[][] getTiles() {
+		return tiles;
+	}
 
-    public Tile[][] getTiles() {
-        return tiles;
+	public void setTile(Tile tile, int x, int y) {
+        tiles[x][y] = tile;
     }
 
     public void setTiles(Tile[][] tiles) {
         this.tiles = tiles;
     }
-    
+
+	/**
+	 * Gets all unoccupied tiles on the board.
+	 *
+	 * @return A set of unoccupied tiles.
+	 */
     public  Set<Tile> getAllUnoccupiedTiles(Tile[][] tiles) {
         Set<Tile> unoccupiedTiles = new HashSet<>();
 
@@ -36,59 +40,8 @@ public class Board {
                 }
             }
         }
-
         return unoccupiedTiles;
     }
-
-	public List<Tile> getAdjacentTiles(Tile tile) {
-		
-	        List<Tile> adjacentTiles = new ArrayList<>();
-
-	        int currentX = tile.getTilex();
-	        int currentY = tile.getTiley();
-
-	        // Check right tile
-	        if (currentX + 1 < 9) {
-	            adjacentTiles.add(tiles[currentX + 1][currentY]);
-	        }
-
-	        // Check left tile
-	        if (currentX - 1 >= 0) {
-	            adjacentTiles.add(tiles[currentX - 1][currentY]);
-	        }
-
-	        // Check bottom tile
-	        if (currentY + 1 < 5) {
-	            adjacentTiles.add(tiles[currentX][currentY + 1]);
-	        }
-
-	        // Check top tile
-	        if (currentY - 1 >= 0) {
-	            adjacentTiles.add(tiles[currentX][currentY - 1]);
-	        }
-
-	        // Check top-right tile
-	        if (currentX + 1 < 9 && currentY - 1 >= 0) {
-	            adjacentTiles.add(tiles[currentX + 1][currentY - 1]);
-	        }
-
-	        // Check top-left tile
-	        if (currentX - 1 >= 0 && currentY - 1 >= 0) {
-	            adjacentTiles.add(tiles[currentX - 1][currentY - 1]);
-	        }
-
-	        // Check bottom-right tile
-	        if (currentX + 1 < 9 && currentY + 1 < 5) {
-	            adjacentTiles.add(tiles[currentX + 1][currentY + 1]);
-	        }
-
-	        // Check bottom-left tile
-	        if (currentX - 1 >= 0 && currentY + 1 < 5) {
-	            adjacentTiles.add(tiles[currentX - 1][currentY + 1]);
-	        }
-
-	        return adjacentTiles;
-	    }
 }
 
 
