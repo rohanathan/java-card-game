90e65fc3c302f7ba7fd0732bd8550e56ed15ba1a
Xuechun Ji <luandun1018@gmail.com>
2025-03-12 03:17:21 +0000

    Refactored Tile

--- app/structures/basic/Tile.java
+++ app/structures/basic/Tile.java
@@ -10,20 +10,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import utils.BasicObjectBuilders;
 
 /**
- * A basic representation of a tile on the game board. Tiles have both a pixel
- * position
- * and a grid position. Tiles also have a width and height in pixels and a
- * series of urls
+ * A basic representation of a tile on the game board. Tiles have both a pixel position
+ * and a grid position. Tiles also have a width and height in pixels and a series of urls
  * that point to the different renderable textures that a tile might have.
- * 
+ *
  * @author Dr. Richard McCreadie
  *
  */
 public class Tile {
 
 	@JsonIgnore
-	private static ObjectMapper mapper = new ObjectMapper(); // Jackson Java Object Serializer, is used to read java
-	// objects from a file
+	private static ObjectMapper mapper = new ObjectMapper(); // Jackson Java Object Serializer, is used to read java objects from a file
 
 	List<String> tileTextures;
 	int xpos;
@@ -32,16 +29,11 @@ public class Tile {
 	int height;
 	int tilex;
 	int tiley;
-	// Tile holds a unit
+	int highlightMode = 0;
 	private Unit unit;
-	// Flag to check the current mode of the tile
-	// i.e., 0: normal, 1: white-highlighted, 2: red-highlighted
-	private int highlightMode = 0;
-	// tracks occupied tiles
-	private boolean occupied = false;
+	private boolean isOccupied = false;
 
-	public Tile() {
-	}
+	public Tile() {}
 
 	public Tile(String tileTexture, int xpos, int ypos, int width, int height, int tilex, int tiley) {
 		super();
@@ -65,99 +57,52 @@ public class Tile {
 		this.tilex = tilex;
 		this.tiley = tiley;
 	}
-
 	public List<String> getTileTextures() {
 		return tileTextures;
 	}
-
 	public void setTileTextures(List<String> tileTextures) {
 		this.tileTextures = tileTextures;
 	}
-
 	public int getXpos() {
 		return xpos;
 	}
-
 	public void setXpos(int xpos) {
 		this.xpos = xpos;
 	}
-
 	public int getYpos() {
 		return ypos;
 	}
-
 	public void setYpos(int ypos) {
 		this.ypos = ypos;
 	}
-
 	public int getWidth() {
 		return width;
 	}
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
 	public int getTilex() {
 		return tilex;
 	}
-
 	public void setTilex(int tilex) {
 		this.tilex = tilex;
 	}
-
 	public int getTiley() {
 		return tiley;
 	}
-
 	public void setTiley(int tiley) {
 		this.tiley = tiley;
-	}
-
-	// added by Renos
-	public Unit getUnit() {
-		return unit;
-	}
-
-	public void setUnit(Unit unit) {
-		if (this.isOccupied()) {
-			System.out.println("Tile is already occupied");
-			return;
-		}
-		this.setOccupied();
-		this.unit = unit;
-	}
-
-	/**
-	 * Removes a Unit from the Tile
-	 */
-	public void removeUnit() {
-		if (this.unit != null) {
-			this.unit = null;
-			this.setNotOccupied();
-		}
-	}
-
-	public int getHighlightMode() {
-		return highlightMode;
-	}
-
-	public void setHighlightMode(int highlightMode) {
-		this.highlightMode = highlightMode;
 	}
 
 	/**
 	 * Loads a tile from a configuration file
 	 * parameters.
-	 *
 	 * @param configFile
 	 * @return
 	 */
@@ -177,13 +122,13 @@ public class Tile {
 	public List<Tile> getAllAdjacentTiles(Board board){
 		List<Tile> tiles = new ArrayList<>();
 
-		//positions of adjacent tiles
+		// Define relative positions of adjacent tiles
 		int[][] directions = {
 				{-1, 0}, {-2, 0}, {1, 0}, {2, 0}, // Left and Right
 				{0, -1}, {0, -2}, {0, 1}, {0, 2}, // Up and Down
 				{-1, 1}, {1, 1}, {-1, -1}, {1, -1} // Diagonals
 		};
-
+		// Iterate through each direction to find adjacent tiles
 		for (int i = 0; i < directions.length; i++) {
 			int newX = tilex + directions[i][0];
 			int newY = tiley + directions[i][1];
@@ -195,12 +140,90 @@ public class Tile {
 		} return tiles;
 	}
 
-	//helper method to check if tile is valid
+	/**
+	 * Checks if a tile position is valid (within the bounds of the board).
+	 *
+	 * @param x The x-coordinate of the tile.
+	 * @param y The y-coordinate of the tile.
+	 * @return True if the tile position is valid, false otherwise.
+	 */
 	boolean isValidTile(int x, int y) {
 		return x >= 0 && x <= 8 && y >=0 && y <=4;
 	}
 
-	public void setOccupied() {
-		this.occupied = true;
+	/**
+	 * Checks if the tile is occupied by a unit.
+	 *
+	 * @return True if the tile is occupied, false otherwise.
+	 */
+	public boolean isOccupied() {
+		return this.isOccupied;
+	}
+
+	/**
+	 * Marks the tile as unoccupied (no unit is present on the tile).
+	 */
+	public void markAsUnoccupied() {
+		this.isOccupied = false;
+	}
+
+	/**
+	 * Marks the tile as occupied (a unit is present on the tile).
+	 */
+	public void markAsOccupied() {
+		this.isOccupied = true;
+	}
+
+	/**
+	 * Gets the unit currently occupying the tile.
+	 *
+	 * @return The unit on the tile, or null if the tile is unoccupied.
+	 */
+	public Unit getUnit() {
+		return unit;
+	}
+
+
+	/**
+	 * Places a unit on the tile if it is not already occupied.
+	 *
+	 * @param unit The unit to place on the tile.
+	 */
+	public void setUnit(Unit unit) {
+		if (this.isOccupied()) {
+			System.out.println("Tile is already occupied. Cannot place unit.");
+			return;
+		}
+		this.markAsOccupied();
+		this.unit = unit;
+		System.out.println("Unit placed successfully on the tile.");
+	}
+
+	/**
+	 * Removes a Unit from the Tile
+	 */
+	public void removeUnit() {
+		if (this.unit != null) {
+			this.unit = null;
+			this.markAsUnoccupied();
+		}
+	}
+
+	/**
+	 * Gets the current highlight mode of the tile.
+	 *
+	 * @return The highlight mode (0: normal, 1: white-highlighted, 2: red-highlighted).
+	 */
+	public int getHighlightMode() {
+		return highlightMode;
+	}
+
+	/**
+	 * Sets the highlight mode of the tile.
+	 *
+	 * @param highlightMode The highlight mode to set (0: normal, 1: white-highlighted, 2: red-highlighted).
+	 */
+	public void setHighlightMode(int highlightMode) {
+		this.highlightMode = highlightMode;
 	}
 }
